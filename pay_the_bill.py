fe62cd8273f1918838de28c0637901e6b31dfc99
ashu <palashu662@gmail.com>
2025-09-04 20:53:18 +0530

    extra spaces

--- pay_the_bill.py
+++ pay_the_bill.py
@@ -1,6 +1,6 @@
 import random
-a=input("enter everybody's name separated by comma:")
-b=a.split(" ")
-d=len(b)
-c=random.randint(0,d-1)
+a = input("enter everybody's name separated by comma:")
+b = a.split(" ")
+d = len(b)
+c = random.randint(0,d-1)
 print(f"{b[c]} will pay the bill.")
